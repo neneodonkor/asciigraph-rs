6671c458cb5cd0d87072497b2e2407a0c5ccad99
Nene Odonkor <38510646+neneodonkor@users.noreply.github.com>
2026-04-19 16:11:29 +0000

    Initial Commit.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add(l: u64, r: u64) -> u64 {
+    l + r
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
